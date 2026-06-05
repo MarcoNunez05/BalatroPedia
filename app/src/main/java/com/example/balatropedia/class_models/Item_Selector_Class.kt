875384f4951167e654b136c22c87352639632845
MarcoNunez05 <mnunez_23@alu.uabcs.mx>
2026-06-05 10:38:49 -0700

    #30 Cambio de nombre y encabezados

--- app/src/main/java/com/example/balatropedia/class_models/Item_Selector_Class.kt
+++ app/src/main/java/com/example/balatropedia/class_models/Item_Selector_Class.kt
@@ -1,3 +1,6 @@
+// Última modificación: 28/05/2026
+// Autor: Marco Núñez
+
 package com.example.balatropedia.class_models
 
 data class ItemSelectorModel(
--- app/src/main/java/com/example/balatropedia/ui/theme/Color.kt
+++ app/src/main/java/com/example/balatropedia/ui/theme/Color.kt
@@ -1,3 +1,6 @@
+// Última modificación: 29/05/2026
+// Autor: Marco Núñez
+
 package com.example.balatropedia.ui.theme
 
 import androidx.compose.ui.graphics.Color
